a92efd9cc70b59d7bc823ee3a0900e8c7c02b91f
Mariana Miguel <mariana.m.miguel@gmail.com>
2020-08-18 17:36:51 -0300

    atualizacao1

---
+++ server/src/controllers/ClassesController.ts
@@ -0,0 +1,102 @@
+import {Request, Response} from 'express';
+import db from '../database/connection';
+import convertHourToMinutes from '../utils/convertHourToMinutes'
+
+
+interface ScheduleItem {
+    week_day:number,
+    from:string,
+    to:string,
+}
+
+export default class ClassesController {
+
+    async index(request:Request, response:Response){
+        const filters = request.query;
+
+        const subject = filters.subject as string;
+        const week_day = filters.week_day as string;
+        const time = filters.time as string;
+
+        if(!filters.week_day || !filters.subject || !filters.time){
+            return response.status(400).json({ 
+                error: 'Missing filters to search classes'})
+        }
+
+        const timeInMinutes = convertHourToMinutes(time)
+        
+        const classes = await db('classes')
+            .whereExists(function(){
+                this.select('classes_schedule.*')
+                    .from('classes_schedule')
+                    .whereRaw('`classes_schedule`.`class_id` = `classes`.`id`')
+                    .whereRaw('`classes_schedule`.`week_day` = ??', [Number(week_day)])
+                    .whereRaw('`classes_schedule`.`from` <= ??', [timeInMinutes])
+                    .whereRaw('`classes_schedule`.`to` > ??', [timeInMinutes])
+            })
+            .where('classes.subject', '=', subject)
+            .join('users', 'classes.user_id', '=','users.id')
+            .select(['classes.*', 'users.*']);
+
+        return response.json(classes);
+    
+    }
+
+
+    async  create (request: Request, response:Response) {
+        const {
+            name,
+            avatar,
+            whatapp,
+            bio, 
+            subject,
+            cost,
+            schedule
+        } = request.body;
+        
+    
+        const trx = await db.transaction();
+    
+    try{
+        const insertedUsersIds = await trx('users').insert({
+            name,
+            avatar,
+            whatapp,
+            bio,
+        });
+    
+        const user_id = insertedUsersIds[0];
+    
+        const insertedCassesIds = await trx('classes').insert({
+            subject,
+            cost,
+            user_id
+        });
+    
+        const class_id = insertedCassesIds[0];
+    
+        const classSchedule = schedule.map((scheduleItem: ScheduleItem ) => {
+            return{
+                class_id,
+                week_day: scheduleItem.week_day,
+                from: convertHourToMinutes(scheduleItem.from),
+                to: convertHourToMinutes(scheduleItem.to),
+            };
+        }); 
+    
+        await trx('classes_schedule').insert(classSchedule);
+    
+        await trx.commit();
+    
+        return response.status(201).send();
+    }catch (err) {
+    
+        await trx.rollback();
+        console.error();
+        return response.status(400).json({
+            error: 'Unexpected erro while ceating new class'
+        })
+    }
+    
+    }
+}
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,56 +1,16 @@
 import express from 'express';
-import db from './database/connection';
-import convertHourToMinutes from './utils/convertHourToMinutes';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
+
 
 const routes = express.Router();
+const classesControllers = new ClassesController();
+const connectionsControllers = new ConnectionsController();
 
-interface ScheduleItem {
-    week_day:number,
-    from:string,
-    to:string
-}
+routes.post('/classes', classesControllers.create);
+routes.get('/classes', classesControllers.index);
 
-routes.post('/classes', async (request, response) =>{
-    const {
-        name,
-        avatar,
-        whatapp,
-        bio, 
-        subject,
-        cost,
-        schedule
-    } = request.body;
-    
-    const insertedUsersIds = await db('users').insert({
-        name,
-        avatar,
-        whatapp,
-        bio,
-    });
-
-    const user_id = insertedUsersIds[0];
-
-    const insertedCassesIds = await db('classes').insert({
-        subject,
-        cost,
-        user_id
-    });
-
-    const class_id = insertedCassesIds[0];
-
-    const classSchedule = schedule.map((scheduleItem: ScheduleItem )=> {
-        return{
-            class_id,
-            week_day: scheduleItem.week_day,
-            from: convertHourToMinutes(scheduleItem.from),
-            to: convertHourToMinutes(scheduleItem.to),
-        }
-    } ) 
-
-    await db('class_schedule').insert(classSchedule);
-
-    return response.send();
-
-});
+routes.post('/connections', connectionsControllers.create);
+routes.get('/connections', connectionsControllers.index);
 
 export default routes;
